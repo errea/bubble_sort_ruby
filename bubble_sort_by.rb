119901f1ebc45a580c9b699ad545ff13a9dcec46
John Arboleda <JohnJack0126@gmail.com>
2021-03-17 21:43:30 -0500

    delete len variable and comments

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,12 +1,9 @@
 def bubble_sort_by(arr)
-  len = arr.length
-  # the array must have 2+ items if not, return the array
-
   # Checks if array is ordered
   is_ordered = false
   until is_ordered # if array is not ordered
     is_ordered = true
-    (0..(len - 2)).each do |i| # the second to last item is the length minus 2
+    (0..(arr.length - 2)).each do |i| # the second to last item is the length minus 2
       if yield(arr[i], arr[i + 1]).positive? # compare items
         arr[i], arr[i + 1] = arr[i + 1], arr[i] # swap items
         is_ordered = false
